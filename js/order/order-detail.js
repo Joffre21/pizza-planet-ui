7608c0674fdbb3623c0468f61771f1f3ac1d5070
Joffre21 <90282920+Joffre21@users.noreply.github.com>
2024-04-11 10:38:20 -0500

    feat/JV: PIZ2 - Adding the update for sizes and beverages

--- js/order/order-detail.js
+++ js/order/order-detail.js
@@ -9,7 +9,19 @@ fetch(`http://127.0.0.1:5000/order/id/${_id}`)
     .then(response => response.json())
     .then(order => {
         let template = createRowTemplate(order);
+        let beverages = [];
+        let ingredients = [];
+        order['detail'].forEach(element => {
+            if(element['beverage_price'] != null)
+                beverages.push(element)
+        });
+        order['detail'].forEach(element => {
+            if(element['ingredient_price'] != null)
+                ingredients.push(element)
+        });
         $("#order").append(template);
+        setBeveragesInHTML(beverages);
+        setIngredientsInHTML(ingredients);
     });
 
 /**
@@ -19,4 +31,20 @@ fetch(`http://127.0.0.1:5000/order/id/${_id}`)
 function createRowTemplate(order) {
     let template = $("#order-template")[0].innerHTML;
     return Mustache.render(template, order);
+}
+
+function setBeveragesInHTML(beveragesList) {
+    let message = ""
+    beveragesList.map(item => {
+        message = message.concat(`<p>${item['beverage']['name']} - \$${item['beverage_price']}</p>`);
+    });
+    document.getElementById('beverages').innerHTML = message;
+}
+
+function setIngredientsInHTML(ingredientList) {
+    let message = ""
+    ingredientList.map(item => {
+        message = message.concat(`<p>${item['ingredient']['name']} - \$${item['ingredient_price']}</p>`);
+    });
+    document.getElementById('ingredients').innerHTML = message;
 }
